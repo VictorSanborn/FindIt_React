a85d3c9e8b6d0765597b6cea8bb1440242261c3c
Victor Sanborn <victor_sanborn@live.se>
2018-04-18 15:33:23 +0200

    init push - Basic map structure and "start" files

---
+++ src/useCase/index/screen/Index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TestTexts from '../container/TestTexts';
+
+class Index extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TestTexts />
+      </div>
+    );
+  }
+}
+
+export default Index;
