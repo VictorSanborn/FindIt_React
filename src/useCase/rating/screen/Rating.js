fd490f1ae3d9152a16df0800d26f1b468099a2fc
Paulina Johnsson <paulina_johnsson@hotmail.com>
2018-04-24 09:42:10 +0200

    Mappstruktur

---
+++ src/useCase/rating/screen/Rating.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import TestTexts from '../container/TestTexts';
+
+class Rating extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TestTexts />
+      </div>
+    );
+  }
+}
+
+export default Rating;
