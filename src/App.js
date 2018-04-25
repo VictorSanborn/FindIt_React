18dbf8fcdd25e177709f505e8e0aed5776898ed8
Victor Sanborn <victor_sanborn@live.se>
2018-04-25 09:18:42 +0200

    Added Boostrap 4, header + body color

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {
   Route,
   Link
 } from 'react-router-dom'
-import logo from './common/resource/logo.svg';
 import './App.css';
 import Index from './useCase/index/screen/Index';
 import Reklam from './useCase/reklam/screen/Reklam';
@@ -14,23 +13,49 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+          <header className="App-header">           
+            <nav class="navbar navbar-expand-lg">
+              <h1 class="navbar-brand App-title" href="#">View It</h1>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
 
-          <ul style={{listStyle: 'none'}}>
-            <li>
-              <Link to="/">Home</Link>&nbsp;
-              <Link to="/reklam">Reklam</Link>&nbsp;
-            </li>
-          </ul>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/reklam">Reklam</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                  </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+              </div>
+            </nav>
           </header>
             
-            <div>
-              { /*Lägg till alla sidor som skall kunna navigeras ifrån!*/ }
-              <Route exact path="/" component={Index}/>
-              <Route path="/reklam" component={Reklam}/>
-            </div>  
+          <div class="col-sm-8 offset-sm-2" style={{backgroundColor: 'white'}}>
+            { /*Lägg till alla sidor som skall kunna navigeras ifrån!*/ }
+            <Route exact path="/" component={Index}/>
+            <Route path="/reklam" component={Reklam}/>
+          </div>  
         </div>
       </Router>
     );
