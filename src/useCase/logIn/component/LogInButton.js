7564a198b603fd956fad230c592e31891ec55be1
henni <henni021@live.se>
2018-05-02 12:54:15 +0200

    log in 2

--- src/useCase/logIn/component/LogInButton.js
+++ src/useCase/logIn/component/LogInButton.js
@@ -5,7 +5,7 @@ class LogInButton extends Component {
   render() {
     return (
         <div className = "App">
-          <button className = "btncolor" type = {this.props.behavior}>{this.props.children}</button>
+          <button className = "btn btn-main" type = {this.props.behavior}>{this.props.children}</button>
       </div>
     );
   }
