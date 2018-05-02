9072c7ef127241ddb33f31c492a9f778653270a4
aggge12 <axlu0001@student.hv.se>
2018-05-02 13:40:51 +0200

    #18 Created rest api and an API class which consumes a SOAP service

---
+++ SoapToRest/SoapToRest/Controllers/ProductServiceController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SoapToRest.Controllers
+{
+    public class ProductServiceController : ApiController
+    {
+
+        ProductService.ProductServiceClient client = new ProductService.ProductServiceClient();
+
+        // GET: /ProductService/GetAllProducts?showDiscounted=false
+        public ProductService.Product[] GetAllProducts(bool showDiscounted)
+        {
+            ProductService.Product[] products = client.GetAllProducts(false);
+            return products;
+        }
+
+        // Productservice/GetAllCategories
+        public ProductService.Category[] GetAllCategories()
+        {
+            ProductService.Category[] categories = client.GetProductCategories();
+            return categories;
+        }
+
+        public ProductService.PriceReport[] GetPricesFromProductAtCompany(int productId, int businessId)
+        {
+            ProductService.PriceReport[] priceReports = client.GetPricesFromProductAtCompany(productId, businessId);
+            return priceReports;
+        }
+    }
+}
