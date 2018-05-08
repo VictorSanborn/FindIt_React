746c8d6a3def95b408f52702a9c39fc4a3047633
unknown <axel.lundberg.gbg@gmail.com>
2018-05-08 14:34:11 +0200

    #40 Change formatters to json from xml

--- SoapToRest/SoapToRest/Global.asax.cs
+++ SoapToRest/SoapToRest/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -15,6 +16,10 @@ namespace SoapToRest
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            GlobalConfiguration.Configuration.Formatters.Clear();
+            GlobalConfiguration.Configuration.Formatters.Add(new JsonMediaTypeFormatter());
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
