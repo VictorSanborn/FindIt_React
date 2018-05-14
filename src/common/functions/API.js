a55809e045fd25d78790b94fe267386ccf52dec4
unknown <axel.lundberg.gbg@gmail.com>
2018-05-14 15:17:17 +0200

    #13 changed serverURi to a variable

--- src/common/functions/API.js
+++ src/common/functions/API.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
+let serverUri = 'http://localhost:5470';
 
 export const loginUser = (username, password) => {
   try {
-    return axios.get('http://localhost:5470/User/GetUserID?username='+username+'&password='+password+'');
+    return axios.get(serverUri + '/User/GetUserID?username='+username+'&password='+password+'');
   } catch (error) {
     console.error(error);
   }
