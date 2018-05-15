0dfff728be1ad914aa205a0dcd098181a4ccf71d
Victor Sanborn <victor_sanborn@live.se>
2018-05-15 11:00:52 +0200

    #REmoved unneeded redux and changed to state tmp storage instead

--- src/useCase/modalLogin/container/ModalHeader.js
+++ src/useCase/modalLogin/container/ModalHeader.js
@@ -2,16 +2,11 @@ import React, { Component } from 'react';
 import { actionCreators } from '../../../common/redux/reducer';
 
 class ModalHeader extends Component {
-
-  removeLogInBoxDataFunction = () => {
-    this.props.dispatch(actionCreators.removeLoginBoxData(''));
-  }
-
   render() {
     return (
       <div class="modal-header App-header">
         <h5 class="modal-title App-title" id="exampleModalLongTitle">{this.props.title}</h5>
-        <button onClick={this.removeLogInBoxDataFunction} type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true" class="App-title">&times;</span>
         </button>
       </div>  
