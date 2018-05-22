9942eadfbe9f936cdf68ec8e71dfddcc117e7e97
unknown <axel.lundberg.gbg@gmail.com>
2018-05-22 17:03:19 +0200

    #108

--- src/useCase/search/container/ResultField.js
+++ src/useCase/search/container/ResultField.js
@@ -13,14 +13,15 @@ class ResultField extends Component {
 
 
   render() {
+    console.log(this.props.price + this.props.businessForProduct) 
     return (
         <div className="row">
             <div class="col-sm-12">
                 {this.props.dataField == "Verksamhet"? <img src={this.props.imgsrc} class="OrganisationImage"></img> : ""}
                 <h5 style={{margin: '10px'}} class="col-sm-6 text-left">{this.props.title}</h5>
                 <div style={{margin: '10px'}} class="col-sm-6 text-left">{this.props.description}</div>
-                {this.props.dataField == "Öl" ? 
-                    this.props.price !== null && this.props.businessForProduct ?
+                {this.props.dataField == "Öl" ?
+                    this.props.price != null && this.props.businessForProduct ?
                         <div style={{margin: '10px'}} class="col-sm-6 text-left">{this.props.price}kr hos <a href={'/Business/' + this.props.businessForProduct.id}>{this.props.businessForProduct.name}</a></div>
                         :
                         <div style={{margin: '10px', color: "#ff0000"}} class="col-sm-6 text-left">Inget pris rapporterat på den här produkten</div>
