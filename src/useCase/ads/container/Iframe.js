d6d38d2286de794e3c6c905dd9e6c2c6b89e1eb7
unknown <axel.lundberg.gbg@gmail.com>
2018-05-28 18:35:27 +0200

    #77 ads

--- src/useCase/ads/container/Iframe.js
+++ src/useCase/ads/container/Iframe.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import Iframe from 'react-iframe';
-import { getAd } from '../../../common/functions/API';
+import {getAd} from '../../../common/functions/API'
 
 class iFrame extends Component {
   constructor(props) {
     super(props);
     this.state = {
       adUrl: '',
+      data: {},
     }
+    this.fetchAd();
   }; 
+
+  fetchAd = () => {
+    getAd().then((response) => {
+      console.log(response);
+      this.setState({
+        data: response.data[0],
+      })
+    })
+  }
 
   render() {
     return (
-      <div className="App">
-        <Iframe url={getAd()}
-          width="600px"
-          height="100px"/>
+      <div className="adsBox">
+         <a
+          title={this.state.data.onHooverText}
+          > {this.state.data.resource} </a>
+
       </div>
     );
   }
