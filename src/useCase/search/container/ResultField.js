baa01f589d49302c32ea788d95acd96f44ddd3dc
unknown <axel.lundberg.gbg@gmail.com>
2018-05-29 17:18:11 +0200

    #124 products should now auto update in list

--- src/useCase/search/container/ResultField.js
+++ src/useCase/search/container/ResultField.js
@@ -6,11 +6,22 @@ class ResultField extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            businessForProduct: ''
+            businessForProduct: '',
+            price: '',
         }
       }; 
 
 
+    componentDidUpdate = (prevProps, prevState) => {
+        if (this.props.dataField === "Dryck"){
+            if (this.props.businessForProduct !== prevProps.businessForProduct){
+                console.log("UPDATE BUSINESS");
+            }
+            if (this.props.price !== prevProps.price){
+                console.log("UPDATE PRICE");
+            }
+        }
+    }
 
   render() {
     console.log(this.props.price + this.props.businessForProduct) 
@@ -21,8 +32,8 @@ class ResultField extends Component {
                 <h5 style={{margin: '10px'}} class="col-sm-6 text-left">{this.props.title}</h5>
                 <div style={{margin: '10px'}} class="col-sm-6 text-left">{this.props.description}</div>
                 {this.props.dataField == "Dryck" ?
-                    this.props.price != null && this.props.businessForProduct ?
-                        <div style={{margin: '10px'}} class="col-sm-6 text-left">{this.props.price}kr hos <a href={'/Business/' + this.props.businessForProduct.id}>{this.props.businessForProduct.name}</a></div>
+                    this.props.price != null && this.props.businessForProduct && this.props.businessForProduct.id ?
+                        <div style={{margin: '10px'}} class="col-sm-6 text-left">{this.props.price} kr hos <a href={'/Business/' + this.props.businessForProduct.id}>{this.props.businessForProduct.name}</a></div>
                         :
                         <div style={{margin: '10px', color: "#ff0000"}} class="col-sm-6 text-left">Inget pris rapporterat på den här produkten</div>
                     
